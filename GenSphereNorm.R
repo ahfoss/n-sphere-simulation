62fc30ed61160449228e23d8b3627b7a4462573e
ahfoss <alexanderhfoss@gmail.com>
2014-07-20 16:49:11 -0400

    Update GenSphereNorm.R
    added library() call

--- GenSphereNorm.R
+++ GenSphereNorm.R
@@ -1,5 +1,7 @@
 # Based on http://stats.stackexchange.com/questions/7977/how-to-generate-uniformly-distributed-points-on-the-surface-of-the-3-d-unit-sphe
 
+library(mvtnorm)
+
 genSphereNorm <- function(nrPoints,nrDim,center=rep(0,nrDim),r=1){
   xs <- rmvnorm(nrPoints,sigma=diag(nrDim))
   lambda <- apply(xs,1,function(tt) sqrt(sum(tt^2)))
