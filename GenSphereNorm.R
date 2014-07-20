4a0d37c38952a7952e5019618b0d79cec73e7613
ahfoss <alexanderhfoss@gmail.com>
2014-07-20 16:36:28 -0400

    Update GenSphereNorm.R

--- GenSphereNorm.R
+++ GenSphereNorm.R
@@ -1,3 +1,5 @@
+# Based on http://stats.stackexchange.com/questions/7977/how-to-generate-uniformly-distributed-points-on-the-surface-of-the-3-d-unit-sphe
+
 genSphereNorm <- function(nrPoints,nrDim,center=rep(0,nrDim),r=1){
   xs <- rmvnorm(nrPoints,sigma=diag(nrDim))
   lambda <- apply(xs,1,function(tt) sqrt(sum(tt^2)))
